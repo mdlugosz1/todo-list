00a98083ac13c0ee9a2b089ee76b56ce06a9d3ce
Michał Długosz <mdugosz@gmail.com>
2021-04-16 16:38:29 +0200

    Add project module

---
+++ src/modules/project.js
@@ -0,0 +1,18 @@
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    setTask(task) {
+        this.tasks.push(task);
+    }
+}
