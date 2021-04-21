da2726362ce574b50bc986dc3450f045098ead99
Michał Długosz <mdugosz@gmail.com>
2021-04-21 13:13:55 +0200

    Add setActive method and refactor reneder project

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,34 @@
 export default class Menu {
-    renederList(list) {
-        const projectsList = document.querySelector('#projects');
+    constructor() {
+        this.menu = document.querySelector('.menu');
+    }
+
+    static setActive(menu) {
+        menu.addEventListener('click', (event) => {
+            const categories = event.target.closest('ul');
+            const uls = menu.querySelectorAll('ul');
+
+            if (categories.id === 'projects') {
+                return;
+            } else if (categories.tagName === 'UL') {
+                for (let i = 0; i < uls.length; i++) {
+                    uls[i].classList.remove('active');
+                }
+
+                categories.className = 'active';
+            }
+        });
+    }
+
+    renederProjectList(list) {
+        const projectsList = this.menu.querySelector('#projects');
 
         for (let item of list) {
             const project = document.createElement('ul');
-            project.textContent = item.name;
+            project.textContent = item;
             projectsList.appendChild(project);
         }
+
+        Menu.setActive(this.menu);
     }
-
-    showTodayTasks() {}
-
-    showWeeklyTasks() {}
 }
