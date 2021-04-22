4dcb536d46c0a905d12a89baa705ac436dfe9037
Michał Długosz <mdugosz@gmail.com>
2021-04-22 22:33:35 +0200

    Add setName and removeTask methods

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,7 +1,14 @@
-class Project {
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Project {
     constructor(name) {
         this.name = name;
         this.tasks = [];
+        this.id = uuidv4();
+    }
+
+    setName(newName) {
+        this.name = newName;
     }
 
     getName() {
@@ -12,7 +19,12 @@ class Project {
         return this.tasks;
     }
 
-    setTask(task) {
+    addTask(task) {
         this.tasks.push(task);
+    }
+
+    removeTask(id) {
+        const taskToRemove = this.tasks.map((task) => task.id).indexOf(id);
+        this.tasks.splice(taskToRemove, 1);
     }
 }
