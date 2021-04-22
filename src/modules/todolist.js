9f3e11d1150de3552eb62ad27fe95c255bdb1a46
Michał Długosz <mdugosz@gmail.com>
2021-04-22 22:35:24 +0200

    Add removeProject and renameProjects methods, remove getTasks and addTask methods, remove tasks list

--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -1,23 +1,29 @@
 export default class ToDoList {
     constructor() {
-        this.projects = ['Become web developer', 'Conquer the world'];
-        this.tasks = [];
+        this.projects = [];
     }
 
     getProjects() {
         return this.projects;
     }
 
-    getTasks() {
-        return this.tasks;
-    }
-
     addProject(project) {
         this.projects.push(project);
     }
 
-    addTask(task) {
-        this.tasks.push(task);
-        console.log(this.projects);
+    removeProject(projectID) {
+        const projectToRemove = this.projects
+            .map((project) => project.id)
+            .indexOf(projectID);
+
+        this.projects.splice(projectToRemove, 1);
+    }
+
+    renameProject(projectID, newName) {
+        this.projects.find(({ id }, index) => {
+            if (projectID === id) {
+                this.projects[index].name = newName;
+            }
+        });
     }
 }
