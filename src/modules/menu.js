847e1fe9823c83bf05440afaa79d778f00432d6e
Michał Długosz <mdugosz@gmail.com>
2021-05-08 13:27:29 +0200

    Refactor menu module

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,23 +1,7 @@
 export default class Menu {
     constructor() {
         this.menu = document.querySelector('.menu');
-    }
-
-    static setActive(menu) {
-        menu.addEventListener('click', (event) => {
-            const categories = event.target.closest('ul');
-            const uls = menu.querySelectorAll('ul');
-
-            if (categories.id === 'projects') {
-                return;
-            } else if (categories.tagName === 'UL') {
-                for (let i = 0; i < uls.length; i++) {
-                    uls[i].classList.remove('active');
-                }
-
-                categories.classList.add('active');
-            }
-        });
+        this.activeElement;
     }
 
     static clearMenu(projects) {
@@ -28,23 +12,71 @@ export default class Menu {
         }
     }
 
+    setActiveElement(element) {
+        const uls = document.querySelectorAll('ul');
+        if (element.id === 'projects') {
+            return;
+        } else if (element.tagName === 'UL') {
+            for (let i = 0; i < uls.length; i++) {
+                uls[i].classList.remove('active');
+            }
+
+            element.classList.add('active');
+            this.activeElement = element;
+        }
+    }
+
     renederProjectList(list) {
         const projectsList = this.menu.querySelector('#projects');
 
         Menu.clearMenu(projectsList);
 
         for (let item of list) {
+            if (item.name === 'Inbox') {
+                continue;
+            }
+
             const project = document.createElement('ul');
-            project.textContent = item;
+            project.textContent = item.name;
+            project.setAttribute('data-project-id', item.id);
+
+            const removeButton = document.createElement('i');
+            removeButton.className = 'far fa-trash-alt';
+            removeButton.setAttribute('data-project-id', item.id);
+
+            project.appendChild(removeButton);
             projectsList.appendChild(project);
         }
-
-        Menu.setActive(this.menu);
     }
 
-    getActiveElement(elements) {
-        if (elements.classList.contains('active')) {
-            return elements.id;
+    addProject(project) {
+        const projectsList = this.menu.querySelector('#projects');
+        const newProject = document.createElement('ul');
+        newProject.textContent = project.name;
+        newProject.setAttribute('data-project-id', project.id);
+        const removeButton = document.createElement('i');
+        removeButton.className = 'far fa-trash-alt';
+        removeButton.setAttribute('data-project-id', project.id);
+        newProject.appendChild(removeButton);
+        projectsList.appendChild(newProject);
+    }
+
+    getActiveElement() {
+        const elementAttribute = this.activeElement.getAttribute(
+            'data-project-id'
+        );
+
+        if (elementAttribute === null) {
+            return this.activeElement.id;
+        } else {
+            return this.activeElement.getAttribute('data-project-id');
         }
     }
+
+    getMenu() {
+        return this.menu;
+    }
+    /* onClick(callback) {
+        this.menu.addEventListener('click', callback);
+    } */
 }
